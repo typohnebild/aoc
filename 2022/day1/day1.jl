600bebaa46c539144c7ab818b40c20dc4da45930
Christoph Alt <typ@ohnebild.com>
2022-12-01 21:38:22 +0100

    maybe a bit of over engineering? at least i am more into julia iterators

--- 2022/day1/day1.jl
+++ 2022/day1/day1.jl
@@ -1,31 +1,22 @@
-function day1_1(file)
-    cur_cal = 0
-    max_cal = 0
-    for line in eachline(file)
-        if line == ""
-            max_cal = max(max_cal, cur_cal)
-            cur_cal = 0
-        else
-            cur_cal += parse(Int64, line)
-        end
-    end
-    max_cal = max(max_cal, cur_cal)
-    println(max_cal)
+import Base
+struct Elf_Iterator
+    file::String
 end
 
-function day1_2(file)
-    cals = Vector{Int64}()
-    cur_cal = 0
-    for line in eachline(file)
-        if line == ""
-            push!(cals, cur_cal)
-            cur_cal = 0
-        else
-            cur_cal += parse(Int64, line)
-        end
+function advance(state::Iterators.Stateful)
+    if !isempty(state)
+        collect(Iterators.takewhile(!=(""), state)), state
     end
-    push!(cals, cur_cal)
-    println(cals)
-    partialsort!(cals, 3, rev=true)
-    println(sum(cals[1:3]))
 end
+
+function Base.iterate(elf_iter::Elf_Iterator)
+    state = Iterators.Stateful(eachline(elf_iter.file))
+    advance(state)
+end
+Base.iterate(_::Elf_Iterator, state::Iterators.Stateful) = advance(state)
+
+parse_snacks(elf) = map(snack -> parse(Int64, snack), elf)
+parse_elfs(file) = Iterators.map(elf -> sum(parse_snacks(elf)), Elf_Iterator(file))
+
+part1(file) = maximum(parse_elfs(file))
+part2(file) = sum(+, partialsort!(collect(Iterators.takewhile(_ -> true, parse_elfs(file))), 1:3, rev=true))
