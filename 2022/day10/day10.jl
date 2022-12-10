a8aa8f20b441e7c9a0faa19b2bd9364710fd9ca1
Christoph Alt <typ@ohnebild.com>
2022-12-10 13:12:40 +0100

    updated day10

--- 2022/day10/day10.jl
+++ 2022/day10/day10.jl
@@ -37,55 +37,35 @@ function update_rule(line::String)
     return parse(Int, split(line, " ")[2]), 2
 end
 
-
+relevant(x::Int) = x in [20, 60, 100, 140, 180, 220]
 
 function part1(file)
-    # cycle = 1
-    # X = 1
     res = 0
-    relevant_cycle = [20, 60, 100, 140, 180, 220]
     for (cycle, X) in enumerate(XIterator(file))
-        if cycle in relevant_cycle
-            println(X, "  ", cycle)
+        if relevant(cycle)
+            # println(X, "  ", cycle)
             res += X * cycle
         end
     end
-    # for line in eachline(file)
-    #     X_update, cycles_to_do = update_rule(line)
-    #     for i in 1:cycles_to_do
-    #         cycle += 1
-    #         if i == cycles_to_do
-    #             X += X_update
-    #         end
-    #         if cycle in relevant_cycle
-    #             println(cycle, " ", X)
-    #             res += X * cycle
-    #         end
-    #     end
-    # end
     res
 end
+
+part1_1(file) = Iterators.filter(x -> relevant(x[1]), enumerate(XIterator(file))) .|> (x -> (x[1] * x[2])) |> sum
+
 printscreen(screen::BitMatrix) = screen .|> (x -> (x ? '#' : '.')) |> x -> join.(eachrow(x))
 
 function part2(file)
-    cycle = 1
-    X = 1
-    screen = falses(6, 40)
-    cur_row = 1
-    for line in eachline(file)
-        X_update, cycles_to_do = update_rule(line)
-        for _ in 1:cycles_to_do
-            crt = (cycle - 1) % 40
-            if abs(crt - X) <= 1
-                screen[cur_row, crt+1] = true
-            end
-            cycle += 1
-            if cycle % 40 == 0
-                cur_row += 1
-            end
+    no_of_cols = 40
+    screen = falses(6, no_of_cols)
+    for (cycle, X) in enumerate(XIterator(file))
+        crt = (cycle - 1) % no_of_cols
+        cur_row = div(cycle, no_of_cols)
+        if abs(crt - X) <= 1
+            screen[cur_row+1, crt+1] = true
         end
-        X += X_update
-
+        if cycle % 40 == 0
+            cur_row += 1
+        end
     end
     screen
 end
